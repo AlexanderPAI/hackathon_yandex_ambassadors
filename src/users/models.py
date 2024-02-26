acbbe1144426ca9861347287730b05905bd9cdd9
Spitsyn Maksim <mgraf@yandex.ru>
2024-02-26 16:11:00 +0300

    add model user

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+ROLE_1 = "ROLE 1"
+ROLE_CHOICES = (
+    (ROLE_1, "Роль 1"),
+)
+
+
+class User(AbstractUser):
+    role = models.CharField(
+        max_length=150, choices=ROLE_CHOICES, default=ROLE_1, verbose_name="Роль"
+    )
+
+    def __str__(self):
+        return self.get_full_name()
