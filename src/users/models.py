2d5eb74abfaaa7ef8deeb54324a19d021c15eed3
Spitsyn Maksim <mgraf@yandex.ru>
2024-02-29 17:56:42 +0300

    edit model user for using email authentication

--- src/users/models.py
+++ src/users/models.py
@@ -3,15 +3,26 @@ from django.contrib.auth.models import AbstractUser
 
 
 ROLE_1 = "ROLE 1"
+ROLE_2 = "ROLE 2"
 ROLE_CHOICES = (
-    (ROLE_1, "Роль 1"),
+    (ROLE_1, "Менеджер"),
+    (ROLE_2, "Амбассадор"),
 )
 
 
 class User(AbstractUser):
-    role = models.CharField(
-        max_length=150, choices=ROLE_CHOICES, default=ROLE_1, verbose_name="Роль"
+    email = models.EmailField(
+        verbose_name="email address",
+        unique=True,
     )
+    role = models.CharField(
+        verbose_name="Роль",
+        max_length=150,
+        choices=ROLE_CHOICES,
+        default=ROLE_1,
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.get_full_name()
