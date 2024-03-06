48cb8a198a427c76f7c74913dfcb2656f712627a
AlexanderPAI <lawlikeart@yandex.ru>
2024-03-06 13:44:27 +0300

    Models have been created

--- src/content/models.py
+++ src/content/models.py
@@ -1,3 +1,240 @@
-# from django.db import models
+from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+from ambassadors.models import Ambassador
+
+
+class GuideTask(models.Model):
+    """Модель такси для гайда."""
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    type = models.CharField(
+        max_length=200,
+        verbose_name='Тип',
+    )
+
+    class Meta:
+        verbose_name = 'Задача для гайда'
+        verbose_name_plural = 'Задачи для гайдов'
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class GuideKit(models.Model):
+    """Модель набора тасок."""
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название набора тасок',
+    )
+    tasks = models.ManyToManyField(
+        GuideTask,
+        through='GuideTaskGuideKit',
+        related_name='guide_kits',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Набор задач для гайда'
+        verbose_name_plural = 'Наборы задач для гайдов'
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class GuideTaskGuideKit(models.Model):
+    """Вспомогательная модель для M2M."""
+    guide_kit = models.ForeignKey(
+        GuideKit,
+        on_delete=models.CASCADE,
+    )
+    task = models.ForeignKey(
+        GuideTask,
+        on_delete=models.CASCADE,
+    )
+
+
+class GuideStatus(models.Model):
+    """Модель статуса гайда."""
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Статус гайда',
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+    )
+
+    class Meta:
+        verbose_name = 'Статус гайда'
+        verbose_name_plural = 'Статусы гайдов'
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        "Автоматический slug."
+        if not self.slug:
+            self.slug = slugify(self.name, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Guide(models.Model):
+    ambassador = models.ForeignKey(
+        Ambassador,
+        on_delete=models.CASCADE,
+        related_name='guides',
+        verbose_name='Амбассадор',
+    )
+    guide_kit = models.ForeignKey(
+        GuideKit,
+        on_delete=models.CASCADE,
+        related_name='guides',
+        verbose_name='Набот тасок',
+    )
+    status = models.ForeignKey(
+        GuideStatus,
+        on_delete=models.SET_NULL,
+        related_name='guides',
+        blank=True,
+        null=True,
+        verbose_name='Статус',
+    )
+    # Сделать автоподсчет
+    task_count = models.IntegerField()
+    counter = models.IntegerField()
+
+
+class MerchPhoto(models.Model):
+    """Модель фото амбассадора в мерче."""
+    ambassador = models.ForeignKey(
+        Ambassador,
+        on_delete=models.CASCADE,
+        related_name='merch_photo',
+        verbose_name='Амбассадор',
+    )
+    photo = models.ImageField(
+        upload_to='content/',
+        blank=True,
+        verbose_name='Фото в мерче'
+    )
+
+    class Meta:
+        verbose_name = 'Фотография в мерче'
+        verbose_name_plural = 'Фотограции в мерче'
+        ordering = ["id"]
+
+    def __str__(self):
+        return f'Фото {self.ambassador.name}'
+
+
+class ReviewPlatfrom(models.Model):
+    """Модель платформы отзыва."""
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название платформы',
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+    )
+
+    class Meta:
+        verbose_name = 'Платформа для отзыва'
+        verbose_name_plural = 'Платформы для отзыва'
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        "Автоматический slug."
+        if not self.slug:
+            self.slug = slugify(self.name, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    """Модель отзыва амбассадора."""
+    ambassador = models.ForeignKey(
+        Ambassador,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Амбассадор'
+    )
+    platform = models.ForeignKey(
+        ReviewPlatfrom,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Платформа',
+    )
+    link = models.URLField(
+        verbose_name='Ссылка',
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв',
+        verbose_name_plural = 'Отзывы'
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"Отзыв {self.ambassador.name} на {self.platform.name}"
+
+
+class ContentPlatform(models.Model):
+    """Модель платформы контента."""
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название платформы',
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='slug',
+    )
+
+    class Meta:
+        verbose_name = 'Платформа контента'
+        verbose_name_plural = 'Платформы контента'
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        "Автоматический slug."
+        if not self.slug:
+            self.slug = slugify(self.name, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Content(models.Model):
+    """Модель контента."""
+    ambassador = models.ForeignKey(
+        Ambassador,
+        on_delete=models.CASCADE,
+        related_name='contents',
+        verbose_name='Амбассадор',
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания',
+    )
+    platform = models.ForeignKey(
+        ContentPlatform,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Платформа',
+    )
+    link = models.URLField(
+        verbose_name='Ссылка',
+    )
+    is_guide_content = models.BooleanField(
+        default=False,
+        verbose_name='Контент в рамках гайда',
+    )
