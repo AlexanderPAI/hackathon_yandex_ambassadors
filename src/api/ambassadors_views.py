e2857901bc99814187d5ccc9a3cf67d88fc4bf9e
Ilya Kotneko <ilya.kotenko@gmail.com>
2024-03-09 19:21:36 +0300

    Add sorting, filtering and searching for ambassadors

--- src/api/ambassadors_views.py
+++ src/api/ambassadors_views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
 from .mixins import DestroyWithPayloadMixin
@@ -9,8 +11,22 @@ from api.ambassadors_serializers import (
 
 
 class AmbassadorViewSet(DestroyWithPayloadMixin, ModelViewSet):
+    """ViewSet for Ambassadors
+    By default sorted by created date (created field) from new to old.
+    Sorting by fields: "created", "email", "phone_number", "telegram_id".
+    Searching by field: "name".
+    Filtering by fields: "status", "name", "gender", "onboarding_status",
+    "program", "country", "city", "activity".
+    """
+
     queryset = Ambassador.objects.all()
     serializer_class = AmbassadorReadSerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter,)
+    filter_fields = ("status", "name", "gender", "onboarding_status", "program",
+                     "country", "city", "activity",)
+    search_fields = "name"
+    ordering_fields = ("created", "email", "phone_number", "telegram_id",)
 
     serializer_action_classes = {
         "list": AmbassadorReadSerializer,
@@ -26,4 +42,7 @@ class AmbassadorViewSet(DestroyWithPayloadMixin, ModelViewSet):
             return super().get_serializer_class()
 
     def get_queryset(self):
-        return AmbassadorReadSerializer.setup_eager_loading(Ambassador.objects.all())
+        queryset = AmbassadorReadSerializer.setup_eager_loading(
+            Ambassador.objects.all()
+        )
+        return queryset.order_by("-created")
