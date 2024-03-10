b56f818ff4269727cd29261dd1fca71df8bafe2c
Ilya Kotneko <ilya.kotenko@gmail.com>
2024-03-10 17:47:13 +0300

    Ambassadors models bugfix, add content field

--- src/ambassadors/models.py
+++ src/ambassadors/models.py
@@ -162,16 +162,15 @@ class Ambassador(models.Model):
         related_name="ambassadors",
         verbose_name="Программа в Практикуме",
     )
-    purpose = models.ForeignKey(
+    purpose = models.ManyToManyField(
         Purpose,
-        on_delete=models.PROTECT,
-        null=True,
-        blank=False,
+        through="AmbassadorPurpose",
         related_name="ambassadors",
         verbose_name="Цель в Практикуме",
     )
 
     about_me = models.TextField(null=True, blank=True, verbose_name="О себе")
+    comment = models.TextField(null=True, blank=True, verbose_name="Комментарий")
 
     class Meta:
         verbose_name = "Амбассадор"
@@ -179,7 +178,7 @@ class Ambassador(models.Model):
 
     def __str__(self):
         return (
-            f"{self.name} ({self.status}) {self.tutor} {self.program} " f"{self.email}"
+            f"{self.name} ({self.status}) {self.tutor} {self.program} {self.email}"
         )
 
 
@@ -193,4 +192,16 @@ class AmbassadorActivity(models.Model):
     activity = models.ForeignKey(
         Activity,
         on_delete=models.CASCADE,
+    )
+
+
+class AmbassadorPurpose(models.Model):
+    """Describe Ambassador and Purpose relations"""
+
+    ambassador = models.ForeignKey(
+        Ambassador, on_delete=models.CASCADE
+    )
+
+    purpose = models.ForeignKey(
+        Purpose, on_delete=models.CASCADE
     )
